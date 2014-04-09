7f09f5236ba2b64c3a1e6387a07e33b6d6b41ff0
Charles Goddard <chargoddard@gmail.com>
2014-04-09 19:48:35 -0400

    Match based on x4x string in titles

--- cupid.py
+++ cupid.py
@@ -14,19 +14,60 @@ def main(city='boston'):
 	corpus = []
 	for i, post in enumerate(craigslist.postings(city)):
 		corpus.append(post)
-		if i >= 49: break
+		if i >= 499: break
 
 	print 'Fetched', len(corpus), 'posts'
+
+	# Categorize posts by gender
+	posts_gender = {}
+	for i, post in enumerate(corpus):
+		post.idx = i
+		code = post.title.split(' - ')[-1]
+		try:
+			gender, seeking = code.split('4')
+		except ValueError:
+			print code
+			post.gender = '??'
+			post.seeking = '??'
+			continue
+		post.gender = gender
+		post.seeking = seeking
+
+		if not gender in posts_gender:
+			posts_gender[gender] = []
+		posts_gender[gender].append(post)
 
 	documents = [c.title + '\n' + c.body for c in corpus]
 	print 'Got all text'
 
-	vectorizer = CountVectorizer(stop_words='english')
-	X = vectorizer.fit_transform(documents)
-	num_samples, num_features = X.shape
+	vectorizer = TfidfVectorizer(stop_words='english')
+	tfidf = vectorizer.fit_transform(documents)
+	print
+	print tfidf[0].A[0]
+	print
+	num_samples, num_features = tfidf.shape
 	print '{0} samples, {1} features'.format(num_samples, num_features)
 	print vectorizer.get_feature_names()
-	print(X)
+	print
+
+	# similarities =  (X * X.T).A - numpy.eye(len(corpus))
+	# for i, post in enumerate(corpus):
+	# 	other_idx = numpy.argmax(similarities[i])
+	# 	other_post = corpus[other_idx]
+	# 	print str(post), '->', str(other_post)
+
+	for i, post in enumerate(corpus):
+		try:
+			candidates = [c for c in posts_gender[post.seeking] if c.idx != i and c.seeking in post.gender]
+		except KeyError:
+			candidates = []
+		if not candidates:
+			print str(post), '-> shit out of luck'
+			continue
+
+		dot_products = numpy.array([sum(tfidf[i].A[0] * tfidf[c.idx].A[0]) for c in candidates])
+		idx = numpy.argmax(dot_products)
+		print str(post), '->', str(candidates[idx])
 
 
 if __name__ == '__main__':
